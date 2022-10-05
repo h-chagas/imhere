d386e8540574e7f9c7d7518ae85986be1911701a
Hugo Chagas <hugochagasuk@gmail.com>
2022-10-05 22:22:09 +0100

    create Header component and its style

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,17 @@
+import { Text, View } from "react-native";
+import { styles } from "./styles";
+
+export function Header() {
+    return (
+        <View 
+        style={styles.container}
+        >
+            <Text 
+            style={styles.text}
+            >
+                imhere
+            </Text>
+        </View>
+    )
+}
+
---
+++ src/components/Header/styles.ts
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+    container: {
+        backgroundColor: '#67159C',
+        height: 60,
+    },
+    text: {
+        fontSize: 24,
+        color: '#fff',
+        textAlign: 'center',
+        justifyContent: 'center',
+        paddingTop: 16,
+    }
+})
